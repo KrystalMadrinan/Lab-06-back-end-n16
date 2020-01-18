cbe04d8adace9b0f185c38ff24e3ea9ef652e308
KRYSTAL MADRINAN <KRYSTAL.MADRINAN@GMAIL.COM>
2020-01-18 14:41:20 -0800

    server changes

--- server/server.js
+++ server/server.js
@@ -17,35 +17,35 @@ app.get('/', (request, response) => {
   response.send('home page!');
 });
 
-app.get('/about', aboutUsHandler);
+// app.get('/about', aboutUsHandler);
 
-function aboutUsHandler(request, response) {
-  response.status(200).send('About us page');
-}
+// function aboutUsHandler(request, response) {
+//   response.status(200).send('About us page');
+// }
 
 
-// lab route stuff
-app.get('/location', (request, response) => {
-  try {
-    //Getting info for object
-    const geoData = require('./data/geo.json');
-    const city = request.query.city;
-    const locationData = new Location(city, geoData);
-    response.send(locationData);
-  } catch (error) {
-    //error function
-  }
+// // lab route stuff
+// app.get('/location', (request, response) => {
+//   try {
+//     //Getting info for object
+//     const geoData = require('./data/geo.json');
+//     const city = request.query.city;
+//     const locationData = new Location(city, geoData);
+//     response.send(locationData);
+//   } catch (error) {
+//     //error function
+//   }
 
-});
-//routes above function below
+// });
+// //routes above function below
 
-//creating object
-function Location(city, geoData) {
-  this.search_query = city;
-  this.formatted_query = geoData[0].display_name;
-  this.latitude = geoData[0].lat;
-  this.longitute = geoData.results[0].lon;
-}
+// //creating object
+// function Location(city, geoData) {
+//   this.search_query = city;
+//   this.formatted_query = geoData[0].display_name;
+//   this.latitude = geoData[0].lat;
+//   this.longitute = geoData.results[0].lon;
+// }
 
 
 // Ensure the server is listening for requests
